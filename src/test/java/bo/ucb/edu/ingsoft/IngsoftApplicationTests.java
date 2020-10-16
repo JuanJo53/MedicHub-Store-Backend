6b20ce65914d12810af0087150e5b4fcde8ea6ea
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2020-10-15 20:20:28 -0400

    first commit

---
+++ src/test/java/bo/ucb/edu/ingsoft/IngsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.ingsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
