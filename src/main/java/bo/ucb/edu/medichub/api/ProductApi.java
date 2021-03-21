9d2ec27a1b5dd3d66d388007cf2eb0f82b5e5a69
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-03-21 00:54:38 -0400

    update and delete of product

--- src/main/java/bo/ucb/edu/medichub/api/ProductApi.java
+++ src/main/java/bo/ucb/edu/medichub/api/ProductApi.java
@@ -6,6 +6,7 @@ import bo.ucb.edu.medichub.dto.ProductRequest;
 import bo.ucb.edu.medichub.model.Transaction;
 import bo.ucb.edu.medichub.util.TransactionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,5 +32,23 @@ public class ProductApi {
         transactionBl.createTransaction(transaction);
         ProductRequest productResponse = productBl.createProduct(productRequest, transaction);
         return productResponse;
+    }
+
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ProductRequest updateProduct(@RequestBody ProductRequest productRequest, HttpServletRequest request) {
+        Transaction transaction = TransactionUtil.createTransaction(request);
+        transactionBl.createTransaction(transaction);
+        ProductRequest productResponse = productBl.updateProduct(productRequest, transaction);
+        return productResponse;
+    }
+
+    @DeleteMapping(path="/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpStatus deletePharmacy(@PathVariable String productId, HttpServletRequest request){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+        transactionBl.createTransaction(transaction);
+        productBl.deleteProduct(Integer.parseInt(productId),transaction);
+        return HttpStatus.ACCEPTED;
     }
 }
--- src/main/java/bo/ucb/edu/medichub/dao/ProductDao.java
+++ src/main/java/bo/ucb/edu/medichub/dao/ProductDao.java
@@ -1,9 +1,14 @@
 package bo.ucb.edu.medichub.dao;
 
+import bo.ucb.edu.medichub.model.Pharmacy;
 import bo.ucb.edu.medichub.model.Product;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProductDao {
     public void  createProduct(Product product);
+
+    public void updateProduct(Product product);
+
+    public void deleteProduct(Product product);
 }
