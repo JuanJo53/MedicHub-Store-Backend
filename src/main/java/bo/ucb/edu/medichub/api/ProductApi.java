342fbe72e5aa40d47fd1718c076c572e60f77ef5
jantonio2 <jesusrochavedia@gmail.com>
2021-03-23 09:01:54 -0400

    Put products response fixed

--- src/main/java/bo/ucb/edu/medichub/api/ProductApi.java
+++ src/main/java/bo/ucb/edu/medichub/api/ProductApi.java
@@ -9,6 +9,7 @@ import bo.ucb.edu.medichub.util.TransactionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,24 +41,24 @@ public class ProductApi {
 
     @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public HttpStatus updateProduct(@Valid @RequestBody ProductRequest productRequest, HttpServletRequest request,
+    public ResponseEntity updateProduct(@Valid @RequestBody ProductRequest productRequest, HttpServletRequest request,
                                         BindingResult result) {
         if(!result.hasErrors()){
             Transaction transaction = TransactionUtil.createTransaction(request);
             transactionBl.createTransaction(transaction);
             ProductRequest productResponse = productBl.updateProduct(productRequest, transaction);
-            return HttpStatus.OK;
+            return new ResponseEntity(productResponse, HttpStatus.OK);
         } else {
-            return HttpStatus.BAD_REQUEST;
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
 
     @DeleteMapping(path="/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HttpStatus deletePharmacy(@PathVariable String productId, HttpServletRequest request){
+    public ResponseEntity deletePharmacy(@PathVariable String productId, HttpServletRequest request){
         Transaction transaction = TransactionUtil.createTransaction(request);
         transactionBl.createTransaction(transaction);
         productBl.deleteProduct(Integer.parseInt(productId),transaction);
-        return HttpStatus.ACCEPTED;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping(path="/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
