3aaec1cdcb03c2e70cfc62b4b73e408bf431dbf1
jantonio2 <jesusrochavedia@gmail.com>
2021-03-24 11:44:34 -0400

    Some inserts on person and admin entities done

---
+++ MedicHubStore-Inserts.sql
@@ -0,0 +1,4 @@
+insert into person values (null, "Andres", "Gomez", "Obrador", "1236884 LP", "+591 62954637", 1, 0,"127.0.0.1",0,"2021-03-24");
+insert into person values (null, "Manuel", "Andrada", "Perez", "951489 LP", "+591 2496853", 1, 0,"127.0.0.1",0,"2021-03-24");
+
+insert into admin values (null, 1, "andres@gmail.com", "andrecito", "abc12345", null, 1, 0,"127.0.0.1",0,"2021-03-24");
