faeed82de9ae841c18600a0b8ac4523da42e74f0
jantonio2 <jesusrochavedia@gmail.com>
2021-04-02 13:36:03 -0400

    ResourceServerConfiguration added

--- src/main/java/bo/ucb/edu/medichub/BackendApplicaction.java
+++ src/main/java/bo/ucb/edu/medichub/BackendApplicaction.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplicaction {
+public class BackendApplicaction{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplicaction.class, args);
--- src/main/java/bo/ucb/edu/medichub/util/security/SpringSecurityConfig.java
+++ src/main/java/bo/ucb/edu/medichub/util/security/SpringSecurityConfig.java
@@ -3,9 +3,12 @@ package bo.ucb.edu.medichub.util.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -30,5 +33,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
